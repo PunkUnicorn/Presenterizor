99dbef09b8b2e81db7f269142aa5d642e79cc8bc
Matthew <PunkUnicorn100@hotmail.com>
2019-08-08 22:09:59 +0100

    proof of concept

--- background.js
+++ background.js
@@ -9,31 +9,69 @@ if(chrome.storage){
 		chrome.browserAction.setIcon({path: path});
 	});
 }
+
+
+//https://developer.chrome.com/extensions/messaging
+
+
 //{'value': theValue}
 //if (chrome.browserAction) {
 	//listen for clicks to turn extension on and off / sets boolean in chrome storage
 	chrome.browserAction.onClicked.addListener(function(tab){
+		
 		chrome.storage.local.get(["presenterizorActivated", "presenterizorIntervalId"], function(value){
+			
 				var presenterizorActivated = !value.presenterizorActivated;
+				
 				if (value.presenterizorIntervalId != -1) {
 					clearInterval(value.presenterizorIntervalId);					
 				}
 				
 				if (presenterizorActivated) {
+					
 					var newIntervalId = setInterval(function() {
+						
+						chrome.tabs.highlight({tabs:1}, function (tabs) { console.log(tabs); });
+						
+						chrome.windows.getCurrent({'populate':true}, function (window) { console.log(window); } );
+						
+						var scrollOptions = {
+							//left: leftInput.value,
+							top: 200,
+							behavior: 'auto' // or 'smooth'
+						}  
+						
+						chrome.tabs.executeScript({
+							//							code: 'window.scrollTo({' + JSON.stringify(scrollOptions) + '})'
+
+							code: 'window.scrollTo(' + JSON.stringify(scrollOptions) + ')'
+						});
+						
 						var d = new Date();
+						
 						console.log('YEAH ' + d.toLocaleTimeString());
+						
 					}, 1000);							
+				}
+				else {
+					
+					newIntervalId = -1;
+					
 				}
 				
 				chrome.storage.local.set({"presenterizorActivated": presenterizorActivated, "presenterizorIntervalId":newIntervalId},
+				
 					function(){
+						
 						var path = "icon.png";
+						
 						if(presenterizorActivated){
+							
 							path = "icon-on.png";
-
+							
 						}
-						chrome.browserAction.setIcon({path: path});						
+						
+						chrome.browserAction.setIcon({path: path});												
 					});
 		});
 	});
